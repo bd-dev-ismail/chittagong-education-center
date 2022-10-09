bbbba69a451c0f66b617780728967db13948c6f1
bd-dev-ismail <ismailratul7@gmail.com>
2022-10-10 00:10:25 +0600

    start new Project

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+  
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <ToastContainer position='top-center'/>
     <App />
   </React.StrictMode>
 );
